6ddf7ce6b75cb20bb2d34859dd49df5d92beb8a4
zhangwei0929 <zhangwei2014_mail@163.com>
2017-12-31 22:11:25 +0800

    pratice

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
@@ -3,6 +3,7 @@ package com.hencoder.hencoderpracticedraw7.practice.practice03;
 import android.animation.ObjectAnimator;
 import android.animation.TypeEvaluator;
 import android.content.Context;
+import android.graphics.Point;
 import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.view.View;
@@ -48,11 +49,14 @@ public class Practice03OfObjectLayout extends RelativeLayout {
     }
 
     private class PointFEvaluator implements TypeEvaluator<PointF> {
+        PointF outPooint = new PointF();
 
         // 重写 evaluate() 方法，让 PointF 可以作为属性来做动画
         @Override
         public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
-            return startValue;
+            outPooint.x = startValue.x + (endValue.x - startValue.x) * fraction;
+            outPooint.y = startValue.y + (endValue.y - startValue.y) * fraction;
+            return outPooint;
         }
     }
 }
